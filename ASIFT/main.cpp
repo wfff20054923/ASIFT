6222922e2089a0fae365e269e46b7cad93e8fd4c
MrWang <mrwang@MrWangdeMacBook-Pro.local>
2015-08-17 09:48:11 +0800

    Initial Commit

---
+++ ASIFT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ASIFT
+//
+//  Created by MrWang on 15/8/17.
+//  Copyright (c) 2015年 WangFei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
